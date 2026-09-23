c5f5905b5fbb201b8517bdb9daa5e86dc1001bdc
Anoopkraju <anoopkraju1897@gmail.com>
2026-09-23 18:13:14 -0400

    Delete Polymorphism/EmployeeIQuittable directory

--- Polymorphism/EmployeeIQuittable/IQuittable.cs
+++
@@ -1,9 +0,0 @@
-namespace EmployeeIQuittable
-{
-    // Define a contract for classes that can perform a Quit action.
-    public interface IQuittable
-    {
-        // Require an implementation that returns no value.
-        void Quit();
-    }
-}
