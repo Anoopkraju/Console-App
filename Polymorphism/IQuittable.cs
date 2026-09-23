b9b73ce432a4d23cfb36ce48f26ce15df15594e0
Anoopkraju <anoopkraju1897@gmail.com>
2026-09-23 18:14:05 -0400

    Add files via upload

---
+++ Polymorphism/IQuittable.cs
@@ -0,0 +1,9 @@
+namespace EmployeeIQuittable
+{
+    // Define a contract for classes that can perform a Quit action.
+    public interface IQuittable
+    {
+        // Require an implementation that returns no value.
+        void Quit();
+    }
+}
